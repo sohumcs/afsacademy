c26d42fc4fae50307a5756a8040737d1b78f8cf1
Sohum Chandra Srivastava <sohum611@gmail.com>
2025-01-19 03:55:38 +0530

    sex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Use Link for navigation
-import '../styles/Navbar.css';
-import logo from "C:/Users/ASUS/Desktop/afsacademy/afsacademy/src/media/logo.jpg";
+import React from "react";
+import { Link } from "react-router-dom"; // Use Link for navigation
+import "../styles/Navbar.css";
+import logo from "C:/Users/Sohum Srivastava/Desktop/afsacademy/afsacademy/src/media/logo.jpg";
 
 const Navbar = ({ toggleTheme, theme }) => {
   return (
@@ -9,7 +9,6 @@ const Navbar = ({ toggleTheme, theme }) => {
       <div className="navbar-left">
         {/* Logo as Home link */}
         <Link to="/" className="navbar-logo-link">
-          {/* Ensure this path is correct */}
           <img src={logo} alt="Logo" className="navbar-logo" />
         </Link>
       </div>
@@ -17,7 +16,7 @@ const Navbar = ({ toggleTheme, theme }) => {
       <div className="navbar-links">
         <Link to="/batches">Batches</Link>
         <Link to="/team">Team</Link>
-        <li><Link to="/achievements">Achievements</Link></li>
+        <Link to="/achievements">Achievements</Link>
         <Link to="/contact">Contact</Link>
       </div>
     </nav>
